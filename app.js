5203b9b73bf5af1a6ca0fc0fec316578ac99f6b6
Huynh Quang Vi <huynhquangvi260705@gmail.com>
2026-09-24 13:34:14 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const contactsRouter = require("./app/routes/contact.route");
+const ApiError = require("./app/api-error");
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -8,4 +9,15 @@ app.get("/", (req, res) => {
     res.json({ message: "Welcome to contact book application." });
 });
 app.use("/api/contacts", contactsRouter);
+// Handle 404 response (Khi route không tồn tại)
+app.use((req, res, next) => {
+    return next(new ApiError(404, "Resource not found"));
+});
+
+// Middleware xử lý lỗi tập trung
+app.use((err, req, res, next) => {
+    return res.status(err.statusCode || 500).json({
+        message: err.message || "Internal Server Error",
+    });
+});
 module.exports = app;
